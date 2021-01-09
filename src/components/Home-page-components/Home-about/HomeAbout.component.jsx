bb92e9be95b1978e3aa4fad9e39d8d619d8afcda
unknown <mateuszwrycza3@gmail.com>
2021-01-09 19:30:06 +0100

    added CustomButton component to HomeAbout component

--- src/components/Home-page-components/Home-about/HomeAbout.component.jsx
+++ src/components/Home-page-components/Home-about/HomeAbout.component.jsx
@@ -1,17 +1,19 @@
 import './homeabout.scss';
 import HomeHeader from '../Home-header/HomeHeader.component'
+import CustomButton from '../../CustomButton/CustomButton.component'
 
 const HomeAbout = () => {
     return(
         <div className='home-about'>
         <HomeHeader header={'Briefly about us'}/>
-            <div className='home-about-content'>
-                <p>
-                    The company was established by combining experienced producers "GUZMET" and "MARKMET" operating on the market for over 30 years.
-                    Specialization of the management staff, production, modern machinery and the company's mission are the factors that allowed for the introduction of high-quality containers with high aesthetic and operational values.
-                    Since 2000, we have been systematically increasing the range of the offered containers and today we offer a full package to meet the needs of the market.
-                </p>
-            </div>
+        <div className='home-about-content'>
+            <p>
+                The company was established by combining experienced producers "GUZMET" and "MARKMET" operating on the market for over 30 years.
+                Specialization of the management staff, production, modern machinery and the company's mission are the factors that allowed for the introduction of high-quality containers with high aesthetic and operational values.
+                Since 2000, we have been systematically increasing the range of the offered containers and today we offer a full package to meet the needs of the market.
+            </p>
+        </div>
+        <CustomButton name={'Company'} UrlLink='/company'/>
         </div>
     )
 }
