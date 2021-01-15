e7be94892ee8aa5c4af6380d0457feea91106ae0
unknown <mateuszwrycza3@gmail.com>
2021-01-15 20:44:41 +0100

    connected to temporary server

--- src/components/Products-components/Product-item/Product-item.component.jsx
+++ src/components/Products-components/Product-item/Product-item.component.jsx
@@ -27,7 +27,7 @@ const ProductItem = ({match}) => {
             redirect: 'follow'
           };
         try {
-            const response = await fetch(`http://localhost:3001/mgbs`, requestOptions)
+            const response = await fetch(`https://cryptic-tor-49247.herokuapp.com/mgbs`, requestOptions)
             const data = await response.json();
             console.log(data)
             setProductDetails(data.ProductDetails)
